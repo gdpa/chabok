12a9cba2d60818f75abe5dd80fe0773df3c15c6c
Masoud Amini <amini141@gmail.com>
2020-05-20 12:32:55 +0430

    Update ChabokChannel.php

--- src/ChabokChannel.php
+++ src/ChabokChannel.php
@@ -19,7 +19,7 @@ class ChabokChannel
     public function __construct(Client $client)
     {
         $this->client = $client;
-        self::$API_ENDPOINT = 'https://' . config('services.chabok.app_id') . '.push.adpdigital.com/api/push/toUsers';
+        self::$API_ENDPOINT = 'https://' . config('services.chabok.app_id') . '.push.adpdigital.com/api/push/notifyUsers';
     }
 
     /**
